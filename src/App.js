6a7014e1930fbb674e67f50fb2cafafc77041056
belferink1996 <ben.elferink@icloud.com>
2020-10-20 19:40:48 +0300

    revert test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  return <div className='app'>under construction test then remove test</div>;
+  return <div className='app'>under construction</div>;
 }
 
 export default App;
