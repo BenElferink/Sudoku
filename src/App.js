df87b7c28b84d18d0d702bbbd2beadf01aca8d00
belferink1996 <ben.elferink@icloud.com>
2020-10-23 19:52:49 +0300

    retrieved timer functions from old files

--- src/App.js
+++ src/App.js
@@ -1,50 +1,96 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import StartPage from './components/StartPage';
 import GamePage from './components/GamePage';
 import ScorePage from './components/ScorePage';
 
-function App() {
-  const [startPage, setStartPage] = useState(false);
-  const [name, setName] = useState('');
-  const [difficulty, setDifficulty] = useState('normal');
-  const [gamePage, setGamePage] = useState(true);
-  const [timer, setTimer] = useState('00:00');
-  const [hints, setHints] = useState(3);
-  const [scorePage, setScorePage] = useState(false);
+export class App extends Component {
+  constructor(props) {
+    super(props);
 
-  const handleHintClick = () => {
-    if (hints > 0) {
-      setHints(hints - 1);
+    this.state = {
+      startPage: true,
+      name: '',
+      difficulty: 'normal',
+      gamePage: false,
+      timer: '00:00',
+      hints: 3,
+      scorePage: false,
+    };
+  }
+
+  handleStartGame = () => {
+    if (this.state.name !== '') {
+      this.setState({ startPage: false, gamePage: true }, this.startTimer);
     }
   };
 
-  return (
-    <>
-      {startPage ? (
-        <StartPage
-          name={name}
-          nameChange={(e) => {
-            setName(e.target.value);
-          }}
-          diff={difficulty}
-          diffChange={(e) => {
-            setDifficulty(e.target.value);
-          }}
-          startGame={null}
-        />
-      ) : null}
-      {gamePage ? (
-        <GamePage
-          timer={timer}
-          hints={hints}
-          hintClick={handleHintClick}
-          resetClick={null}
-          abortClick={null}
-        />
-      ) : null}
-      {scorePage ? <ScorePage /> : null}
-    </>
-  );
+  handleHintClick = () => {
+    if (this.state.hints > 0) {
+      this.setState({ hints: this.state.hints - 1 });
+    }
+  };
+
+  render() {
+    return (
+      <>
+        {this.state.startPage ? (
+          <StartPage
+            name={this.state.name}
+            nameChange={(e) => {
+              this.setState({ name: e.target.value });
+            }}
+            diff={this.state.difficulty}
+            diffChange={(e) => {
+              this.setState({ difficulty: e.target.value });
+            }}
+            startGame={this.handleStartGame}
+          />
+        ) : null}
+        {this.state.gamePage ? (
+          <GamePage
+            timer={this.state.timer}
+            hints={this.state.hints}
+            hintClick={this.handleHintClick}
+            resetClick={null}
+            abortClick={null}
+          />
+        ) : null}
+        {this.state.scorePage ? <ScorePage /> : null}
+      </>
+    );
+  }
+
+  // ---------- //
+  // TIMER FUNCTIONS //
+  // ---------- //
+
+  timerInterval;
+
+  // START TIMER
+  startTimer = () => {
+    let minutes = '00';
+    let seconds = '00';
+    this.timerInterval = setInterval(() => {
+      if (seconds < 60) {
+        seconds++;
+        if (seconds < 10) {
+          seconds = '0' + seconds;
+        }
+        if (seconds === 60) {
+          minutes++;
+          if (minutes < 10) {
+            minutes = '0' + minutes;
+          }
+          seconds = '00';
+        }
+      }
+      this.setState({ timer: `${minutes}:${seconds}` });
+    }, 1000);
+  };
+
+  stopTimer = () => {
+    clearInterval(this.timerInterval);
+  };
 }
 
 export default App;
