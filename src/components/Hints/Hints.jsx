45dba672c17e8a49f98145b280a59c5da8049dab
belferink1996 <ben.elferink@icloud.com>
2020-10-30 22:07:22 +0200

    slightly refined hints component

--- src/components/Hints/Hints.jsx
+++ src/components/Hints/Hints.jsx
@@ -1,32 +1,28 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { updateMatrix } from './../../redux/actions';
 import { hintsUse } from './../../redux/actions/hintsUse';
+import { updateMatrix } from './../../redux/actions';
 import Button from './../Button/Button';
 import Icon from './LightBulb-48px';
 import './Hints.css';
 
 export default function Hints() {
-  const matrixOriginal = useSelector((state) => state.matrix.original);
-  const matrixPlayed = useSelector((state) => state.matrix.played);
   const hints = useSelector((state) => state.hints.remaining);
+  const matrix = useSelector((state) => state.matrix);
   const dispatch = useDispatch();
 
   const clickHint = () => {
     if (hints > 0) {
-      let loop = true;
-      while (loop) {
+      while (true) {
         let i = Math.floor(Math.random() * 9);
         let j = Math.floor(Math.random() * 9);
-        if (matrixPlayed[i][j] === '') {
-          let revealNumber = matrixOriginal[i][j];
+        if (matrix.played[i][j] === '') {
+          let revealNumber = matrix.original[i][j];
           dispatch(updateMatrix(revealNumber, i, j));
-          loop = false;
+          break;
         }
       }
       dispatch(hintsUse());
-    } else {
-      console.log('no hints left');
     }
   };
 
