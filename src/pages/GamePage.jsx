9c27bfb2a37126b239553063b72e080a2bd68091
belferink1996 <ben.elferink@icloud.com>
2020-11-01 21:50:00 +0200

    slight cleanup

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -13,8 +13,6 @@ import './style/style.css';
 export default function GamePage() {
   const difficulty = useSelector((state) => state.difficulty);
   const playerData = useSelector((state) => state.playerData);
-  const time = playerData.time;
-  const hints = playerData.hints;
   const matrix = useSelector((state) => state.matrixData);
   const dispatch = useDispatch();
 
@@ -34,8 +32,8 @@ export default function GamePage() {
   return (
     <div className='page'>
       <div className='page-content'>
-        <Timer time={time} />
-        <Hints hints={hints} matrix={matrix} />
+        <Timer time={playerData.time} />
+        <Hints hints={playerData.hints} matrix={matrix} />
         <GameBoard matrix={matrix} handleFinish={handleFinish} />
       </div>
       <div className='btn-bar'>
